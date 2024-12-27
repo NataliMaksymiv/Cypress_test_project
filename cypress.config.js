9432cda3c95f864f1047b2a30780f9d8c9377826
nmaksy <naty.maksymiv@gmail.com>
2024-12-27 15:02:29 +0200

    18 hw added

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,17 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) { },
-    baseUrl: "http://localhost: 3000",
+    //baseUrl: 'https://guest:welcome2qauto@qauto.forstudy.space/',
+    //retries:{
+    //   runMode: 2,
+    //   openMode: 3
+    //},
     defaultCommandTimeout: 10000,
     pageLoadTimeout: 30000,
-    viewportWidth: 1280,
-    viewportHeight: 720,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    video: true,
+    chromeWebSecurity: false,
+    screenshotOnRunFailure: true
   },
 });
